2dd48e7530796a7b66df280f83e490665867990c
Michael Witwicki <michaelwitwicki@michaels-mbp.lan>
2021-09-07 12:40:37 -0400

    Added cloudflare header into ip lookup

--- src/services/IpstackService.php
+++ src/services/IpstackService.php
@@ -52,7 +52,7 @@ class IpstackService extends Component
      */
     public function getCountry()
     {
-        $ip   = Craft::$app->getRequest()->getUserIP();
+        $ip   = $this->getIpAddress();
         $data = '';
 
         // set IP address and API access key
@@ -85,7 +85,7 @@ class IpstackService extends Component
 
     public function get()
     {
-        $ip   = Craft::$app->getRequest()->getUserIP();
+        $ip   = $this->getIpAddress();
         $data = '';
 
         // set IP address and API access key
@@ -119,22 +119,6 @@ class IpstackService extends Component
 
     private function getIpAddress()
     {
-        //Just get the headers if we can or else use the SERVER global
-        if (function_exists('apache_request_headers')) {
-            $headers = apache_request_headers();
-        } else {
-            $headers = $_SERVER;
-        }
-        //Get the forwarded IP if it exists
-        if (array_key_exists('X-Forwarded-For', $headers) && filter_var($headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-            $the_ip = $headers['X-Forwarded-For'];
-        } elseif (array_key_exists('HTTP_X_FORWARDED_FOR', $headers) && filter_var($headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)
-        ) {
-            $the_ip = $headers['HTTP_X_FORWARDED_FOR'];
-        } else {
-
-            $the_ip = filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-        }
-        return $the_ip;
+        return $_SERVER['HTTP_CF_CONNECTING_IP'] ?? Craft::$app->getRequest()->getUserIP();
     }
 }
